3e7d8e4e377b9bc3ec50bec1069e314421a97da5
Yanivv77 <Yanivv77@gmail.com>
2022-03-05 00:40:05 +0200

    BlogDetails

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar'
 import Home from './Components/Home'
 import Create from './Components/Create'
+import BlogDetails from './Components/BlogDetails'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/create" element={<Create />}></Route>
+            <Route path="/blogs/:id" element={<BlogDetails />}></Route>
           </Routes>
         </div>
       </div>
