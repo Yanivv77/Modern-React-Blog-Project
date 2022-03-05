6c8e94407fc688bd64ab63e7c7bf46c8ad2fdc35
Yanivv77 <Yanivv77@gmail.com>
2022-03-05 20:24:28 +0200

    useFetch BlogDetails

--- src/Components/BlogDetails.js
+++ src/Components/BlogDetails.js
@@ -1,7 +1,9 @@
 import { useParams } from 'react-router-dom'
+import useFetch from './Reusable/useFetch'
 
 const BlogDetails = () => {
   const { id } = useParams()
+  const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id)
 
   return (
     <div className="blog-details">
